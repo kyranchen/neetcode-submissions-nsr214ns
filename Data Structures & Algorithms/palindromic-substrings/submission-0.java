0270bb627664b083c0c96186af146f96cbc70005
Kyran Chen <50513956+kyranchen@users.noreply.github.com>
2026-05-28 11:50:07 +0800

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    private int res = 0;
+    public int countSubstrings(String s) {
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            check(s, i, i);
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            check(s, i, i + 1);
+        }
+
+        return res;
+    }
+
+    private void check(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            res += 1;
+            left -= 1;
+            right += 1;
+        }
+
+        return;
+    }
+}
