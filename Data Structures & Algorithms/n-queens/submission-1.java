ac281bb5197b58fd8512fc333de703302148a709
Kyran Chen <50513956+kyranchen@users.noreply.github.com>
2026-07-02 17:01:40 +0800

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.java
@@ -0,0 +1,52 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<>();
+        char[][] grid = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = '.';
+            }
+        }
+        backtrack(grid, 0, res);
+
+        return res;
+    }
+
+    private void backtrack(char[][] grid, int row, List<List<String>> res) {
+        if (row >= grid.length) {
+            List<String> curr = new ArrayList<>();
+            for (char[] data : grid) {
+                curr.add(new String(data));
+            }
+            res.add(curr);
+            return;
+        }
+
+        for (int j = 0; j < grid.length; j++) {
+            // Start from Q at 0, 0
+            if (isValid(grid, row, j)) {
+                grid[row][j] = 'Q';
+                backtrack(grid, row + 1, res);
+                grid[row][j] = '.';
+            } 
+        }
+    }
+
+    private boolean isValid(char[][] grid, int row, int col) {
+        int n = grid.length;
+
+        for (int i = 0; i < n; i++) {
+            if (grid[i][col] == 'Q' || grid[row][i] == 'Q') return false;
+        }
+        // Check left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (grid[i][j] == 'Q') return false;
+        }
+        // Check right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (grid[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+}
