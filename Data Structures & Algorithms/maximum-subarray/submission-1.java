458de8f676e2b5707a31b916a3ffb98821084719
Kyran Chen <50513956+kyranchen@users.noreply.github.com>
2026-09-03 11:25:57 +0800

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // As we traverse thru the array
+        // When we encounter a number larger than the current sum
+        // If the curr sum is negative, replace the sum
+
+        int n = nums.length;
+        int max = nums[0];
+        int curr = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > curr) {
+                if (curr < 0) {
+                    curr = nums[i];
+                } else {
+                    curr += nums[i];
+                }
+            } else {
+                curr += nums[i];
+            }
+
+            max = Math.max(max, curr);
+        }
+
+        return max;
+    }
+}
